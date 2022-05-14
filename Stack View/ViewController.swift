c406ebb4e3c5a4172b0052ab566fc11d390a816b
ligiaviana <ligia.viana.azevedo@gmail.com>
2022-05-14 17:56:58 -0300

    Initial Commit

---
+++ Stack View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack View
+//
+//  Created by Lígia Viana Azevedo on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
